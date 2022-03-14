e98c852912b2beaa5abab59224d7e5423561b29d
Idris_DY <88961234+IdrisDY@users.noreply.github.com>
2022-03-14 22:20:29 +0100

    Update 7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - main code black
+ * main - print alphabet letters in reverse
  * Description: reverse the letters and printout
- * return: always 0(sucess)
+ * Return: Always 0 (success)
  */
 
 int main(void)
