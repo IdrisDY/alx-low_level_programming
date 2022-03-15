3f9388b0fa493b4529d82098dc6f243023c536f0
Idris adekunle <idrisadekunle726@gmail.com>
2022-03-15 18:52:32 +0100

    func

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ *main - main code block
+ * Desription : print _putchar
+ * return : 0
+ */
+int main(void)
+{
+	printf("_putchar\n")
+
+	return (0);
+}
