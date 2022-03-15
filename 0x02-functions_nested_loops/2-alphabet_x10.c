629a31b58121f8188bc7390041279d53e8bb1d4f
Idris adekunle <idrisadekunle726@gmail.com>
2022-03-15 21:44:36 +0100

    3 alpha

---
+++ 0x02-functions_nested_loops/2-alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+
+*/
+void print_alphabet_x10(void)
+{
+int no = 0;
+while(no < 10){
+
+{
+	char ch;
+
+	ch = 'a';
+
+	while (ch <= 'z')
+	{
+		_putchar(ch);
+		ch++;
+	}
+
+	_putchar('\n');
+}
+
+}
