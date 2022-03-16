3937ef4c67308595cec26fb1e690455a56373984
Idris adekunle <idrisadekunle726@gmail.com>
2022-03-16 17:20:44 +0100

    rb3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _islower - checks lowercase
+ *
+ * c- c is the input character
+ * Return : 1 if lowercase
+ * Return : 0 if not
+ */
+int _islower(int c)
+{
+(if c => "a" && <= "z"){
+	return (1);
+else
+	return (0);
+}
