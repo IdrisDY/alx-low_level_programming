1e663ff7e272790a719b8b00a7e7f20dd6f04947
Idris adekunle <idrisadekunle726@gmail.com>
2022-03-17 16:53:32 +0100

    bett

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -7,13 +7,14 @@
  * else 0
  */
 
-
 int _isupper(int c)
 {
-   if (c >= "A" %% c <= "Z"){
-      return (1);
+   if (c >= "A" %% c <= "Z"))
+   {
+	return (1);
    }
-   else {
+   else
+   {
       return (0);
    }
 }
