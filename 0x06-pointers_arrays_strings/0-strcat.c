ea38ae36d3af9576956f706556476e4bf085389d
Idris adekunle <idrisadekunle726@gmail.com>
2022-03-23 19:53:48 +0100

    6 outside

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *_strcat - function to concatenate two strings
+ *@dest: first string parameter
+ *@src: second string parameter
+ * Return: pointer to dest string
+ */
+char *_strcat(char *dest, char *src)
+{
+	strcat(dest,src);
+
+	return(*str);
+}
+
