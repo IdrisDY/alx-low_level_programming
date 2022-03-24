82bf1c2e693d7f809597d39ba4da6de7e7bbbaf1
Idris adekunle <idrisadekunle726@gmail.com>
2022-03-24 18:43:13 +0100

    mist

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,15 +1,32 @@
-#include <stdio.h>
 #include "main.h"
+
 /**
- *_strcat - function to concatenate two strings
- *@dest: first string parameter
- *@src: second string parameter
- * Return: pointer to dest string
- */
+* *_strcat - function commute srtings
+* @dest: param pointer to a char
+* @src: param pointer to a char
+* Return: return value of dest
+*/
+
 char *_strcat(char *dest, char *src)
 {
-	strcat(dest,src);
+	int i;
+	int j;
 
-	return(*str);
+	i = 0;
+	j = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		j++;
+		i++;
+	}
+
+	dest[i] = '\0';
+	return (dest);
 }
-
