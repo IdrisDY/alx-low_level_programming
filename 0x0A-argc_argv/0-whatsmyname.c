6e24760d1e7ce0c5eda3d9dca0f1e0c3e958cd80
Idris adekunle <idrisadekunle726@gmail.com>
2022-04-03 13:13:22 +0100

    arg[0]

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - program prints its name, with a new line
+ * @argc: first argument
+ * @argv: second argument
+ * Return: 0 on success
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
